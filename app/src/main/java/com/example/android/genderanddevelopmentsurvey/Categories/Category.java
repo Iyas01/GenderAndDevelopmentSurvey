8c3c25123c8ae2e64aacc0538668fccb7584aa60
Cas <casicabal@gmail.com>
2017-07-18 16:50:53 +0800

    working swipeToDelete item on ListView

--- app/src/main/java/com/example/android/genderanddevelopmentsurvey/Categories/Category.java
+++ app/src/main/java/com/example/android/genderanddevelopmentsurvey/Categories/Category.java
@@ -1,15 +1,77 @@
 package com.example.android.genderanddevelopmentsurvey.Categories;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.android.genderanddevelopmentsurvey.Categories.Demography.DemographyOne;
+import com.example.android.genderanddevelopmentsurvey.Categories.EconomicActivity.EconomicActivityOne;
+import com.example.android.genderanddevelopmentsurvey.Categories.EducationAndLiterature.EducationAndLiteratureOne;
+import com.example.android.genderanddevelopmentsurvey.Categories.Health.HealthOne;
+import com.example.android.genderanddevelopmentsurvey.Categories.Participation.ParticipationOne;
 import com.example.android.genderanddevelopmentsurvey.R;
 
-public class Category extends AppCompatActivity {
+public class Category extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        Button demographyBtn = (Button) findViewById(R.id.demographyBtn);
+        Button edalBtn = (Button) findViewById(R.id.edalBtn);
+        Button partiBtn = (Button) findViewById(R.id.partiBtn);
+        Button econoBtn = (Button) findViewById(R.id.econoBtn);
+        Button healthBtn = (Button) findViewById(R.id.healthBtn);
+        Button hocBtn = (Button) findViewById(R.id.hocBtn);
+        Button interBtn = (Button) findViewById(R.id.interBtn);
+
+        demographyBtn.setOnClickListener(this);
+        edalBtn.setOnClickListener(this);
+        partiBtn.setOnClickListener(this);
+        econoBtn.setOnClickListener(this);
+        healthBtn.setOnClickListener(this);
+        hocBtn.setOnClickListener(this);
+        interBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.demographyBtn:
+                Intent intent1 = new Intent(Category.this, DemographyOne.class);
+                startActivity(intent1);
+                break;
+            case R.id.edalBtn:
+                Intent intent2 = new Intent(Category.this, EconomicActivityOne.class);
+                startActivity(intent2);
+                break;
+            case R.id.partiBtn:
+                Intent intent3 = new Intent(Category.this, EducationAndLiteratureOne.class);
+                startActivity(intent3);
+                break;
+            case R.id.econoBtn:
+                Intent intent4 = new Intent(Category.this, HealthOne.class);
+                startActivity(intent4);
+                break;
+            case R.id.healthBtn:
+                Intent intent5 = new Intent(Category.this, ParticipationOne.class);
+                startActivity(intent5);
+                break;
+//            case R.id.hocBtn:
+//                Intent intent6 = new Intent(Category.this, ForthActivityHousingCharacteristic.class);
+//                startActivity(intent6);
+//                break;
+//            case R.id.infoBtn:
+//                Intent intent7 = new Intent(Category.this, ForthActivityHousingCharacteristic.class);
+//                startActivity(intent7);
+//                break;
+//            case R.id.interBtn:
+//                Intent intent8 = new Intent(Category.this, ForthActivityInterviewStatus.class);
+//                startActivity(intent8);
+//                break;
+        }
     }
 }
--- app/src/main/java/com/example/android/genderanddevelopmentsurvey/DMainActivity.java
+++ app/src/main/java/com/example/android/genderanddevelopmentsurvey/DMainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.genderanddevelopmentsurvey;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -17,18 +18,23 @@ import com.example.android.genderanddevelopmentsurvey.Categories.Category;
 
 import java.util.ArrayList;
 
-public class DMainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+public class DMainActivity extends AppCompatActivity {
 
+    public int itemSelected;
     EditText fromUserInput;
     ArrayList<String> arrHouseholdMems = new ArrayList<>();
     ListView showMems;
     String userInput;
+    ArrayAdapter adapter;
+    GestureDetectorCompat swipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dmain);
 
+//   Create a swipe gesture listening event
+        swipe = new GestureDetectorCompat(DMainActivity.this, new swipeToDelete());
 
 //      Create arrayList from user inputs
         fromUserInput = (EditText) findViewById(R.id.et_dmain);
@@ -48,17 +54,15 @@ public class DMainActivity extends AppCompatActivity implements GestureDetector.
                             Toast.LENGTH_SHORT).show();
                 } else {
                     arrHouseholdMems.add(userInput);
-                    ArrayAdapter adapter = new ArrayAdapter(DMainActivity.this, android.R.layout.simple_list_item_1,
+                    adapter = new ArrayAdapter<>(DMainActivity.this, android.R.layout.simple_list_item_1,
                             arrHouseholdMems);
                     showMems.setAdapter(adapter);
                     fromUserInput.setText(userInput);
                     fromUserInput.setText("");
-
                 }
             }
         });
 
-//      TODO:  setOnItemClickListener on each item on the ArrayList
         showMems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -66,37 +70,40 @@ public class DMainActivity extends AppCompatActivity implements GestureDetector.
                 startActivity(intent);
             }
         });
+
+//I will change this "long press to delete", to "Swipe to delete" later. Option: RecycleView
+//        showMems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//                arrHouseholdMems.remove(position);
+//                adapter.notifyDataSetChanged();
+//                return false;
+//            }
+//        });
     }
 
     @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
+    public boolean onTouchEvent(MotionEvent event) {
+        this.swipe.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
-    @Override
-    public void onShowPress(MotionEvent e) {
+    class swipeToDelete extends GestureDetector.SimpleOnGestureListener {
 
-    }
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
 
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
+            if (e2.getX() < e1.getX()) {
 
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
+//                Working on this part
+//                Test this tommorow
+                arrHouseholdMems.remove(onTouchEvent(e1));
+                adapter.notifyDataSetChanged();
 
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-//        code to remove selected item on the arraylist
-
-        return false;
+            } else {
+                // Do nothing on left to right swipe
+            }
+            return true;
+        }
     }
 }
